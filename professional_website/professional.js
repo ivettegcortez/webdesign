cf948d61403684654539bab3a64e84e141fd686c
Ivette <ic923@nyu.edu>
2020-05-07 01:34:21 -0400

    HTML, CSS, JS Update
    These are the updates to this artistic website with the following files.

--- professional_website/professional.js
+++ professional_website/professional.js
@@ -3,14 +3,6 @@
 // have a toggle kind of button where if clicked the section of each heading is shown and scrolled down
 // then if clicked on more info then all the info's text for each art piece will appear
 
-// $(function() {
-//   // when user clicks info element
-//   	$('#info').click(function() {
-//     // hide and show the info for the art
-//     $('.artinfo').display(none);
-//   });
-
-// });
 
 
 var date = new Date();
@@ -21,6 +13,12 @@ var header_color = document.querySelector("header");
 var h2 = document.querySelectorAll("h2");
 var h3 = document.querySelectorAll("h3");
 var i;
+
+let songName = document.getElementById('audiotitle');
+let plusArt = document.getElementById('ArtInfo');
+let artStyle = document.getElementById('Art').style;
+let digitalStyle = document.getElementById('digital').style;
+let plusDigital = document.getElementById('DigitalInfo');
 
 function mtw(){
 	console.log("Day: " + day);
@@ -37,7 +35,7 @@ function mtw(){
 }
 function thf(){
 	console.log("Day: " + day);
-	header_color.style.backgroundColor = "#63d4e2";
+	header_color.style.backgroundColor = "#47c1d1";
 	// light blue
 	header_color = document.querySelector("header").style.backgroundColor = "#63d4e2";
 	for (i = 0; i<h2.length; i++){
@@ -62,37 +60,60 @@ function weekend(){
 	if (day == 1 || day == 2 || day == 3){
 		mtw();
 		if(day == 1){
-			alert("Moody Monday...");
+			songName.textContent = "The Muffin Song";
 			music.src = "audio/muffin-song.mp3";
 			
 		}
 		else if(day == 2){
-			alert("Troubling Tuesday---");
+			songName.textContent = "Black M - Sur Ma Route";
 			music.src = "audio/black-m_sur-ma-route.mp3";
-			
 		}
 		else{
-			alert("Wacky Wednesdays~");
+			songName.textContent = "Danny Ocean - Epa Wei";
 			music.src = "audio/danny-ocean_epa-wei.mp3";
 		}
 	} else if (day == 4 || day == 5){
 		thf();
 		if(day == 4){
-			alert("Tricky Thursday?");
+			songName.textContent = "Pentagon - Naughty Boy";
 			music.src = "audio/pentagon-naughty-boy.mp3";
 		}
 		else{
-			alert("Finally Friday!");
+			songName.textContent = "The Cheetah Girls - Strut";
 			music.src = "audio/cheetah-girls_strut.mp3";
 		}
 	} else if (day == 6){
 		weekend();
-		alert("Silly Saturday?! :0")
+		songName.textContent = "Flo-Rida ft Maluma - Hola";
 		music.src = "audio/florida-maluma_hola.mp3";
 		
 	} else {
 		weekend();
-		alert("Study Sunday??:(");
+		songName.textContent = "Noel - Your Voice";
 		music.src = "audio/noel_your-voice.mp3";
 	}
+
+plusArt.onclick = function reveal(){
+	if(artStyle.display == "none"){
+		artStyle.display = "block";
+		plusArt.textContent = "-"
+		// artStyle.padding = "10em";
+	}else{
+		artStyle.display = "none";
+		plusArt.textContent = "+";
+	}
+}
+
+plusDigital.onclick = function digReveal(){
+	if(digitalStyle.display == "none"){
+		digitalStyle.display = "block";
+		plusDigital.textContent = "-"
+		// artStyle.padding = "10em";
+	}else{
+		digitalStyle.display = "none";
+		plusDigital.textContent = "+";
+	}
+}
+
+
 
